6050a5da8875d1a17b8cd16bf20ccb2343785c0a
redsaeok <74396365+redsaeok@users.noreply.github.com>
2024-03-28 17:39:28 -0700

    Added chart to visually show the loan history

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult LoanHistory()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
